063c670331a9568a10a6e27c1281ffcd0ff699c0
George Morev <morevgeorge@gmail.com>
2025-07-02 21:17:03 +0300

    Add logs to allure

--- utils/validators.py
+++ utils/validators.py
@@ -3,14 +3,21 @@ from pydantic import ValidationError
 
 
 def validate_with_pydantic(response, model):
+    allure.attach(response.text, name="Raw Response", attachment_type=allure.attachment_type.JSON)
     """
     Валидация JSON-ответа с помощью Pydantic модели.
     При ошибке — прикрепляет ошибку в отчет Allure и выбрасывает исключение.
     """
     try:
-        # Парсим тело ответа через Pydantic модель
-        model.parse_raw(response.text)
+        parsed_obj = model.parse_raw(response.text)
     except ValidationError as e:
-        # Добавляем в отчет Allure подробности ошибки валидации
-        allure.attach(str(e), name="ValidationError", attachment_type=allure.attachment_type.TEXT)
+        allure.attach(str(e), name="Pydantic Validation Error", attachment_type=allure.attachment_type.TEXT)
         raise
+
+        # Если всё прошло успешно, логируем результат валидации
+    allure.attach("Ответ успешно валидирован", name="Pydantic Validation", attachment_type=allure.attachment_type.TEXT)
+    allure.attach(
+        parsed_obj.model_dump_json(indent=2),
+        name="Parsed Object",
+        attachment_type=allure.attachment_type.JSON
+    )
